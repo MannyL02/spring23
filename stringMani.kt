2a3ed2c63facf9a42f550eb98b7782ae8d9a46f9
MannyL02 <123099964+MannyL02@users.noreply.github.com>
2023-02-09 09:39:31 -0600

    Update stringMani.kt

--- stringMani.kt
+++ stringMani.kt
@@ -1,2 +1,8 @@
-var city = "dallas";
-println("The length of the city string is: " + city.length);
+// Oscar Banuelos-Lopez, String Manipulator, 2/9/2023
+fun main() {
+var city = "dallas"
+println("The length of the city string is: " + city.length)
+println(city.toUpperCase())
+println(city.toLowerCase())
+  println(city[0]) 
+   }
